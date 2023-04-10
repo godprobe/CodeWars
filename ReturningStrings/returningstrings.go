6d993c769f24ead1cdf692488c82ef4220b97cf8
godprobe <godprobe.misc+github@gmail.com>
2023-04-10 11:55:19 -0700

    Using Sprintf

--- ReturningStrings/returningstrings.go
+++ ReturningStrings/returningstrings.go
@@ -1,5 +1,7 @@
 package kata
 
+import "fmt"
+
 func Greet(name string) string {
-	return "Hello, " + name + " how are you doing today?"
+	return fmt.Sprintf("Hello, %s how are you doing today?", name)
 }
