56668b4af1861de93721f0685c66921a7292f207
godprobe <godprobe.misc+github@gmail.com>
2023-04-10 10:05:57 -0700

    named return value optimization

--- PowersOf2/PowersOf2.go
+++ PowersOf2/PowersOf2.go
@@ -2,10 +2,7 @@ package kata
 
 import "math"
 
-func PowersOfTwo(n int) []uint64 {
-	var (
-		powers []uint64
-	)
+func PowersOfTwo(n int) (powers []uint64) {
 	for i := 0; i <= n; i++ {
 		powers = append(powers, uint64(math.Pow(2, float64(i))))
 	}
