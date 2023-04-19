82a0adece4d1fdb29d5e34be3e48e3595388c61f
godprobe <godprobe.misc+github@gmail.com>
2023-04-19 11:56:09 -0700

    refactor: modulo operator and no type conversions

--- CountTheDivisorsOfANumber/CountTheDivisors.go
+++ CountTheDivisorsOfANumber/CountTheDivisors.go
@@ -1,11 +1,9 @@
 package kata
 
-import "math"
-
 func Divisors(n int) int {
 	var count = 1
-	for f := 0.0; f <= float64(n)/2; f++ {
-		if math.Mod(float64(n), f) == 0 {
+	for f := 1; f <= n/2; f++ {
+		if n%f == 0 {
 			count++
 		}
 	}
