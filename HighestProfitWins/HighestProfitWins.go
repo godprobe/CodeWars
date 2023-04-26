47218fece5cf7a80d682667080c9f018d9878ab0
godprobe <godprobe.misc+github@gmail.com>
2023-04-25 22:36:54 -0700

    refactor: skip redundant 1st item; use else

--- HighestProfitWins/HighestProfitWins.go
+++ HighestProfitWins/HighestProfitWins.go
@@ -2,11 +2,10 @@ package kata
 
 func MinMax(prices []int) [2]int {
 	result := [2]int{prices[0], prices[0]}
-	for _, val := range prices {
+	for _, val := range prices[1:] {
 		if val < result[0] {
 			result[0] = val
-		}
-		if val > result[1] {
+		} else if val > result[1] {
 			result[1] = val
 		}
 	}
