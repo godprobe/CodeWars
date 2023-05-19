cd37d7a3277b6707bfd51767f815d25240b9ea5b
godprobe <godprobe.misc+github@gmail.com>
2023-05-18 23:02:19 -0700

    more optimal, still times out on codewars

--- CountTheDigit/CountTheDigit.go
+++ CountTheDigit/CountTheDigit.go
@@ -6,15 +6,9 @@ import (
 )
 
 func NbDig(n, d int) int {
-	set := make([]int, 0)
-	for i := 0; i <= n; i++ {
-		set = append(set, i*i)
-	}
-	count := 0
 	var allnums string
-	for _, val := range set {
-		allnums += strconv.Itoa(val)
+	for i := 0; i <= n; i++ {
+		allnums += strconv.Itoa(i * i)
 	}
-	count = len(strings.Split(allnums, strconv.Itoa(d))) - 1
-	return count
+	return len(strings.Split(allnums, strconv.Itoa(d))) - 1
 }
