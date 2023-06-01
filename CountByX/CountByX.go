5ec04065d0399e49291c9f0a9d80c5cce6bbac76
godprobe <godprobe.misc+github@gmail.com>
2023-06-01 00:02:33 -0700

    Fix: x and n were reversed in the tests

--- CountByX/CountByX.go
+++ CountByX/CountByX.go
@@ -2,8 +2,8 @@ package kata
 
 func CountBy(x, n int) []int {
 	result := []int{}
-	for i := 1; i <= x; i++ {
-		result = append(result, n*i)
+	for i := 1; i <= n; i++ {
+		result = append(result, x*i)
 	}
 	return result
 }
--- CountByX/CountByX_test.go
+++ CountByX/CountByX_test.go
@@ -21,7 +21,7 @@ countBy(2,5)  should return {2,4,6,8,10}
 var (
 	got, want []int
 	cases     = []struct {
-		n, x   int
+		x, n   int
 		result []int
 	}{
 		{1, 10, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
