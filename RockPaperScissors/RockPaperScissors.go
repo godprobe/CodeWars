a7e7bd861e59abdcf7c07c47cd1aa603e5d1b155
godprobe <godprobe.misc+github@gmail.com>
2023-06-01 23:28:15 -0700

    Refactor: no real reason for any additional vars

--- RockPaperScissors/RockPaperScissors.go
+++ RockPaperScissors/RockPaperScissors.go
@@ -1,16 +1,12 @@
 package kata
 
 func Rps(player1, player2 string) string {
-	rock := "rock"
-	paper := "paper"
-	scissors := "scissors"
-
 	if player1 == player2 {
 		return "Draw!"
 	}
 
 	switch player1 + player2 {
-	case rock + paper, paper + scissors, scissors + rock:
+	case "rockpaper", "paperscissors", "scissorsrock":
 		return "Player 2 won!"
 	}
 
