f644bc3f1895f86545751937092535313dea7962
godprobe <godprobe.misc+github@gmail.com>
2023-06-01 23:11:10 -0700

    Refactor: fewer redundant and/or magic strings

--- RockPaperScissors/RockPaperScissors.go
+++ RockPaperScissors/RockPaperScissors.go
@@ -1,23 +1,30 @@
 package kata
 
 func Rps(player1, player2 string) string {
+	outcome := "Draw!"
+	rock := "rock"
+	paper := "paper"
+	scissors := "scissors"
+
 	if player1 == player2 {
-		return "Draw!"
+		return outcome
 	}
 
+	outcome = "Player 2 won!"
 	switch player1 {
-	case "rock":
-		if player2 == "paper" {
-			return "Player 2 won!"
+	case rock:
+		if player2 == paper {
+			return outcome
 		}
-	case "paper":
-		if player2 == "scissors" {
-			return "Player 2 won!"
+	case paper:
+		if player2 == scissors {
+			return outcome
 		}
-	case "scissors":
-		if player2 == "rock" {
-			return "Player 2 won!"
+	case scissors:
+		if player2 == rock {
+			return outcome
 		}
 	}
-	return "Player 1 won!"
+	outcome = "Player 1 won!"
+	return outcome
 }
