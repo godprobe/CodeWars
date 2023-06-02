d0034884776982b226d7316fb0749dc4d0730af2
godprobe <godprobe.misc+github@gmail.com>
2023-06-01 22:17:45 -0700

    codewars/convertnumbertoreversedarrayofdigits

--- ConvertNumberToReversedArrayOfDigits/ConvertNumber.go
+++ ConvertNumberToReversedArrayOfDigits/ConvertNumber.go
@@ -1,5 +1,27 @@
 package kata
 
+import (
+	"math"
+)
+
 func Digitize(n int) []int {
-	return []int{n}
+	arr := []int{}
+
+	for i := 0; i < lenOfInt(n); i++ {
+		// ((n / 10^i) % 10)
+		arr = append(arr, int(math.Mod(math.Floor(float64(n)/math.Pow(10, float64(i))), 10)))
+	}
+	return arr
+}
+
+func lenOfInt(i int) int {
+	if i == 0 {
+		return 1
+	}
+	count := 0
+	for i != 0 {
+		i /= 10
+		count++
+	}
+	return count
 }
