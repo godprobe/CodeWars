8ddc3c5bbb64bfb9d90337bf57973734484bf451
godprobe <godprobe.misc+github@gmail.com>
2023-06-01 22:26:10 -0700

    Refactor: % operator, do-until style loop, omit math lib, break down n in the loop

--- ConvertNumberToReversedArrayOfDigits/ConvertNumber.go
+++ ConvertNumberToReversedArrayOfDigits/ConvertNumber.go
@@ -1,27 +1,13 @@
 package kata
 
-import (
-	"math"
-)
-
 func Digitize(n int) []int {
-	arr := []int{}
+	var arr []int
 
-	for i := 0; i < lenOfInt(n); i++ {
-		// ((n / 10^i) % 10)
-		arr = append(arr, int(math.Mod(math.Floor(float64(n)/math.Pow(10, float64(i))), 10)))
+	for {
+		arr = append(arr, n%10)
+		n /= 10
+		if n == 0 {
+			return arr
+		}
 	}
-	return arr
-}
-
-func lenOfInt(i int) int {
-	if i == 0 {
-		return 1
-	}
-	count := 0
-	for i != 0 {
-		i /= 10
-		count++
-	}
-	return count
 }
