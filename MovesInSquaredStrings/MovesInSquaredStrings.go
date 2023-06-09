a14d33ddc0e53a2744b30529a775ff550fc1d045
godprobe <godprobe.misc+github@gmail.com>
2023-06-09 02:14:31 -0700

    Working; could use some refactoring

--- MovesInSquaredStrings/MovesInSquaredStrings.go
+++ MovesInSquaredStrings/MovesInSquaredStrings.go
@@ -1,21 +1,42 @@
 package kata
 
+import (
+	"strings"
+)
+
 func VertMirror(s string) string {
-	// your code
-	return "VertFail"
+	split := strings.Split(s, "\n")
+	for i := 0; i < len(split); i++ {
+		split[i] = reverse(split[i])
+	}
+	return strings.Join(split, "\n")
 }
 
 func HorMirror(s string) string {
-	// your code
-	return "HorFail"
+	s = reverse(s)
+	split := strings.Split(s, "\n")
+	for i := 0; i < len(split); i++ {
+		split[i] = reverse(split[i])
+	}
+	return strings.Join(split, "\n")
 }
 
 type FParam func(string) string
 
 func Oper(f FParam, x string) string {
-	// s = "abcd\nefgh\nijkl\nmnop"
-	// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
-	// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
-	// your code
 	return f(x)
+}
+
+func reverse(s string) string {
+	var (
+		temp rune
+	)
+	runes := []rune(s)
+	last := len(runes) - 1
+	for i := 0; i <= last/2; i++ {
+		temp = runes[i]
+		runes[i] = runes[last-i]
+		runes[last-i] = temp
+	}
+	return string(runes)
 }
