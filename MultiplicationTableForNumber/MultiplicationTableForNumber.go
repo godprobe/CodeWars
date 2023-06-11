37a543d06d337fe4200bdc2d8fb228cbd51d3709
godprobe <godprobe.misc+github@gmail.com>
2023-06-10 23:56:01 -0700

    Refactor: same no. of lines, but reduced duplicate code

--- MultiplicationTableForNumber/MultiplicationTableForNumber.go
+++ MultiplicationTableForNumber/MultiplicationTableForNumber.go
@@ -3,9 +3,9 @@ package kata
 import "fmt"
 
 func MultiTable(number int) string {
-	result := fmt.Sprintf("1 * %d = %d", number, number)
-	for i := 2; i <= 10; i++ {
-		result += fmt.Sprintf("\n%d * %d = %d", i, number, i*number)
+	var result string
+	for i := 1; i <= 10; i++ {
+		result += fmt.Sprintf("%d * %d = %d\n", i, number, i*number)
 	}
-	return result
+	return result[:len(result)-1]
 }
