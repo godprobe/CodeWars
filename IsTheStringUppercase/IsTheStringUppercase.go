760f35649c39a31e7942476dd950df9fcf7be52b
godprobe <godprobe.misc+github@gmail.com>
2023-06-13 16:24:23 -0700

    Refactor: removed unnecessary fmt import

--- IsTheStringUppercase/IsTheStringUppercase.go
+++ IsTheStringUppercase/IsTheStringUppercase.go
@@ -1,12 +1,11 @@
 package kata
 
 import (
-	"fmt"
 	"strings"
 )
 
 type MyString string
 
 func (s MyString) IsUpperCase() bool {
-	return strings.ToUpper(fmt.Sprint(s)) == fmt.Sprint(s)
+	return strings.ToUpper(string(s)) == string(s)
 }
