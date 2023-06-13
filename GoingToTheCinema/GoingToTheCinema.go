ddaea1dfcdd471ff1964b6993866d8221e737088
godprobe <godprobe.misc+github@gmail.com>
2023-06-13 01:11:09 -0700

    Minor changes

--- GoingToTheCinema/GoingToTheCinema.go
+++ GoingToTheCinema/GoingToTheCinema.go
@@ -1,20 +1,16 @@
 package kata
 
-import (
-	"fmt"
-	"math"
-)
+import "math"
 
 func Movie(card, ticket int, perc float64) int {
 	i := 0
 	p := perc
-	SystemACost := 0
-	SystemBCost := float64(card)
-	for i = 1; float64(SystemACost) <= math.Ceil(SystemBCost); i++ {
-		SystemACost += ticket
-		SystemBCost += float64(ticket) * p
+	CostA := 0
+	CostB := float64(card)
+	for i = 1; float64(CostA) <= math.Ceil(CostB); i++ {
+		CostA += ticket
+		CostB += float64(ticket) * p
 		p *= perc
-		fmt.Printf("A: %d, \tB: %v, \tTickets: %d\n", SystemACost, SystemBCost, i)
 	}
 	return i - 1
 }
