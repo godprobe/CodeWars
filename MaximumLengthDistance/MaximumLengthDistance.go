e71cec7b0935c29f82b660c2e981bd8e78217b2f
godprobe <godprobe.misc+github@gmail.com>
2023-06-25 00:11:27 -0700

    Various optimizations as suggested by ChatGPT, and a regression bugfix

--- MaximumLengthDistance/MaximumLengthDistance.go
+++ MaximumLengthDistance/MaximumLengthDistance.go
@@ -1,30 +1,28 @@
 package kata
 
-func MxDifLg(a1 []string, a2 []string) (maxDiff int) {
+func MxDifLg(a1 []string, a2 []string) int {
 	if len(a1) == 0 || len(a2) == 0 {
 		return -1
 	}
+
 	a1min, a1max := getMinMaxStrLens(a1)
 	a2min, a2max := getMinMaxStrLens(a2)
-	if a1min != 0 && a2min != 0 {
-		if a1max-a2min > a2max-a2min {
-			maxDiff = a1max - a2min
-		} else {
-			maxDiff = a2max - a1min
-		}
+
+	if a1max-a2min > a2max-a1min {
+		return a1max - a2min
 	}
-	return
+	return a2max - a1min
 }
 
 func getMinMaxStrLens(ax []string) (min, max int) {
 	min = len(ax[0])
-	for _, val := range ax {
-		x := len(val)
-		if x > max {
-			max = x
-		}
-		if x < min {
+	max = len(ax[0])
+	for i := 1; i < len(ax); i++ {
+		switch x := len(ax[i]); {
+		case x < min:
 			min = x
+		case x > max:
+			max = x
 		}
 	}
 	return
