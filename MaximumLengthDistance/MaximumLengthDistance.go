134555f867941fea6135950fdf72707b4779d960
godprobe <godprobe.misc+github@gmail.com>
2023-06-24 23:43:41 -0700

    Linear time complexity, minimal trade-offs

--- MaximumLengthDistance/MaximumLengthDistance.go
+++ MaximumLengthDistance/MaximumLengthDistance.go
@@ -1,16 +1,31 @@
 package kata
 
-import "math"
-
-func MxDifLg(a1 []string, a2 []string) int {
-	maxDiff := -1
-	for _, val1 := range a1 {
-		for _, val2 := range a2 {
-			x := int(math.Abs(float64(len(val1) - len(val2))))
-			if maxDiff < x {
-				maxDiff = x
-			}
+func MxDifLg(a1 []string, a2 []string) (maxDiff int) {
+	if len(a1) == 0 || len(a2) == 0 {
+		return -1
+	}
+	a1min, a1max := getMinMaxStrLens(a1)
+	a2min, a2max := getMinMaxStrLens(a2)
+	if a1min != 0 && a2min != 0 {
+		if a1max-a2min > a2max-a2min {
+			maxDiff = a1max - a2min
+		} else {
+			maxDiff = a2max - a1min
 		}
 	}
-	return maxDiff
+	return
+}
+
+func getMinMaxStrLens(ax []string) (min, max int) {
+	min = len(ax[0])
+	for _, val := range ax {
+		x := len(val)
+		if x > max {
+			max = x
+		}
+		if x < min {
+			min = x
+		}
+	}
+	return
 }
